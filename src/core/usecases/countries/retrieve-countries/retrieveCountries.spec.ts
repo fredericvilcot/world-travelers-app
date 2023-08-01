b32a85a74ea5befd0880835e046238393cf3d116
Frédéric Vilcot <fredericvilcot@gmail.com>
2023-08-01 11:34:54 +0200

    test(core): update test suite

--- src/core/usecases/countries/retrieve-countries/retrieveCountries.spec.ts
+++ src/core/usecases/countries/retrieve-countries/retrieveCountries.spec.ts
@@ -6,16 +6,16 @@ describe("Retrieve  countries", () => {
   let initialState: AppState;
 
   beforeEach(() => {
-    store = configureStore();
+    store = configureStore({});
     initialState = store.getState();
   });
 
   it("should retrieve no countries if there are none available", () => {
-    store.dispatch({ type: "RETRIEVED_SHOPPING_LIST" });
+    store.dispatch({ type: "RETRIEVED_COUNTRIES" });
     expect(store.getState().coreLogicState).toEqual({
       ...initialState.coreLogicState,
-      cityList: {
-        list: null,
+      countries: {
+        data: null,
       },
     });
   });
